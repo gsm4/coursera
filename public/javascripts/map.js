a2cccdeca43f0abbc30ed1432998e8c6a57c636a
Giorbis <gsm4@cin.ufpe.br>
2020-07-20 19:49:56 -0300

    api de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,9 +1,18 @@
-var mymap = L.map('main_map').setView([51.505, -0.09], 13);
+var mymap = L.map('main_map').setView([-8.050692, -34.959616], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: ' &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
 }).addTo(mymap);
 
-L.marker([51.505, -0.09]).addTo(mymap);
-L.marker([51.500, -0.15]).addTo(mymap);
-L.marker([51.495, -0.09]).addTo(mymap);
+L.marker([-8.049002, -34.957637]).addTo(mymap);
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    succes: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+            L.marker([bici.ubicacion]).addTo(mymap);
+        });
+    }
+})
